229c6b070cbfa4d5f03d17f2e95d8b95a5b722f0
drojf <drojfjord@gmail.com>
2019-04-29 15:26:37 +1000

    Add more tests

--- main.c
+++ main.c
@@ -15,6 +15,31 @@ void tearDown(void)
 
 }
 
+void checkInputOutputSameTest(uint8_t * data_to_encode, uint8_t * expected_encoded_data, uint8_t length)
+{
+	if (length == 0 || length >= 255)
+	{
+		printf("Invalid array length: must be between 1 and 254 bytes");
+		TEST_ABORT();
+	}
+
+	uint8_t working_buffer[255];
+
+	// encode data_to_encode into working_buffer
+	encode(data_to_encode, length, working_buffer);
+
+	if (expected_encoded_data != NULL)
+	{
+		TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expected_encoded_data, working_buffer, length + 1, "The encoded input does not match the expected encoded data!");
+	}
+
+	// decode working_buffer in-place. Data starts at working_buffer[1]
+	decode_in_place(working_buffer, length + 1);
+
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(data_to_encode, &working_buffer[1], length, "Decoded output is not the same as input!");
+}
+
+
 void test_Encode254NonzeroBytes(void)
 {
 	uint8_t input_array[NO_ZEROS_TEST_LENGTH];
@@ -23,54 +48,60 @@ void test_Encode254NonzeroBytes(void)
 		input_array[i] = i+1;
 	}
 
-	uint8_t encoded_array[NO_ZEROS_TEST_LENGTH + 1];
+	checkInputOutputSameTest(input_array, NULL, sizeof(input_array));
+}
 
-	encode(input_array, NO_ZEROS_TEST_LENGTH, encoded_array);
+struct TestCase {
+	uint8_t input_length;
+	uint8_t input[255];
+	uint8_t encoded[255];
+};
 
-	// The encoded data should have no 0's
-	for (int i = 0; i < NO_ZEROS_TEST_LENGTH + 1; i++)
+struct TestCase test_cases[] = {
+	{ 
+		.input_length = 1,
+		.input   = { 0x00 }, 
+		.encoded = { 0x01, 0x01 }
+	},
 	{
-		TEST_ASSERT_NOT_EQUAL(0, encoded_array[i]);
-	}
-
-	// The first element should be equal to 255 (to indicate 254 nonzero bytes ahead)
-	TEST_ASSERT_EQUAL_UINT8(255, encoded_array[0]);
-
-	decode_in_place(encoded_array, NO_ZEROS_TEST_LENGTH+1);
-
-	for (int i = 0; i < NO_ZEROS_TEST_LENGTH; i++)
+		.input_length = 2,
+		.input   = { 0x00, 0x00 },
+		.encoded = { 0x01, 0x01, 0x01 },
+	},
 	{
-		TEST_ASSERT_EQUAL_UINT8(input_array[i], encoded_array[i+1]);
+		.input_length = 4,
+		.input   = { 0x11, 0x22, 0x00, 0x33 },
+		.encoded = { 0x03, 0x11, 0x22, 0x02, 0x33 },
+	},
+	{
+		.input_length = 4,
+		.input   = { 0x11, 0x22, 0x33, 0x44 },
+		.encoded = { 0x05, 0x11, 0x22, 0x33, 0x44 },
+	},
+};
+
+void test_Encoded(void)
+{
+	size_t numTestCases = sizeof(test_cases) / sizeof(test_cases[0]);
+
+	for (size_t tc = 0; tc < numTestCases; tc++)
+	{
+		checkInputOutputSameTest(test_cases[tc].input, test_cases[tc].encoded, test_cases[tc].input_length);
 	}
 }
 
 void test_BasicTest(void)
 {
 	uint8_t test_input_array[6] = { 1,2,0,3,0 };
-
-	uint8_t encoded_array[6+1];
-
-	encode(test_input_array, 6, encoded_array);
-
-	// The encoded data should have no 0's
-	for (int i = 0; i < 6; i++)
-	{
-		TEST_ASSERT_NOT_EQUAL(0, encoded_array[i]);
-	}
-
-	decode_in_place(encoded_array, 6+1);
-
-	for (int i = 0; i < 6; i++)
-	{
-		TEST_ASSERT_EQUAL_UINT8(test_input_array[i], encoded_array[i+1]);
-	}
+	checkInputOutputSameTest(test_input_array, NULL, sizeof(test_input_array));
 }
 
 
 int main()
 {
 	UNITY_BEGIN();
-	//RUN_TEST(test_BasicTest);
+	RUN_TEST(test_BasicTest);
 	RUN_TEST(test_Encode254NonzeroBytes);
+	RUN_TEST(test_Encoded);
 	return UNITY_END();
 }
